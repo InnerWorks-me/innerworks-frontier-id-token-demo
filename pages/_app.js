130bb49507311598421af54a72da9bc73a3c549b
Vladimir Haltakov <vladimir.haltakov@gmail.com>
2023-12-13 16:45:59 +0100

    feat: style the page

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,7 @@
 import { SessionProvider } from "next-auth/react";
 
+import "../styles/global.css";
+
 export default function App({ Component, pageProps: { session, ...pageProps } }) {
     return (
         <SessionProvider session={session}>
--- pages/index.js
+++ pages/index.js
@@ -5,34 +5,81 @@ export default function Component() {
     const { data: session } = useSession();
 
     return (
-        <>
+        <div
+            style={{
+                maxWidth: "800px",
+                margin: "0 auto",
+            }}
+        >
             <Head>
-                <title>Sign in with Google</title>
+                <title>ID Token Demo</title>
             </Head>
-            <h1>Sign in with Google</h1>
+
+            <h1 style={{ textAlign: "center" }}>ID Token Demo</h1>
+
+            <p>
+                This is a demo application that allows the user to login using Sign in with Google and obtain an ID
+                token.
+            </p>
 
             {session ? (
                 <>
-                    <div>Signed in as {session.user.email}</div>
-
                     <div>
-                        <h2>OIDC token</h2>
-                        <pre>{session.idToken}</pre>
-                        <button onClick={() => navigator.clipboard.writeText(session.idToken)}>copy</button>
+                        <h2 style={{ textAlign: "center" }}>ID Token</h2>
+
+                        <div style={{ margin: "16px 0px", fontSize: "16px" }}>
+                            Signed in as <b>{session.user.email}</b>
+                        </div>
+
+                        <div
+                            style={{
+                                border: "1px solid #999",
+                                padding: "16px",
+                                backgroundColor: "#eee",
+                                width: "100%",
+                                overflow: "auto",
+                                wordWrap: "break-word",
+                                fontFamily: "monospace",
+                                fontSize: "16px",
+                            }}
+                        >
+                            {session.idToken}
+                        </div>
+                        <button
+                            style={{ display: "block", margin: "16px auto", fontSize: "16px" }}
+                            onClick={() => navigator.clipboard.writeText(session.idToken)}
+                        >
+                            Copy to Clipboard
+                        </button>
                     </div>
 
                     <div>
-                        <h3>Sign Out</h3>
-                        <button onClick={() => signOut()}>Sign out</button>
+                        <button
+                            style={{ fontSize: "24px", display: "block", margin: "64px auto", padding: "8px 32px" }}
+                            onClick={() => signOut()}
+                        >
+                            Sign out
+                        </button>
                     </div>
                 </>
             ) : (
                 <>
                     <div>
-                        <button onClick={() => signIn()}>Sign in</button>
+                        <button
+                            style={{
+                                width: "300px",
+                                margin: "100px auto",
+                                display: "block",
+                                fontSize: "24px",
+                                padding: "8px 32px",
+                            }}
+                            onClick={() => signIn()}
+                        >
+                            Sign in with Google
+                        </button>
                     </div>
                 </>
             )}
-        </>
+        </div>
     );
 }
