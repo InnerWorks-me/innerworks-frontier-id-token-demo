5f267aa72d306bd30ae33407a2f2c7c7c7734afe
Vladimir Haltakov <vladimir.haltakov@gmail.com>
2024-02-10 22:56:04 +0100

    feat: add privacy policy and terms and conditions for Google verification

---
+++ pages/privacy.js
@@ -0,0 +1,162 @@
+export default function Privacy() {
+  return (
+    <div>
+      <h1>Privacy Policy</h1>
+      <p>Last updated: February 10, 2024</p>
+      <p>
+        This Privacy Policy describes Our policies and procedures on the collection, use and disclosure of Your
+        information when You use the Service and tells You about Your privacy rights and how the law protects You.
+      </p>
+      <h2>Interpretation and Definitions</h2>
+      <h3>Interpretation</h3>
+      <p>
+        The words of which the initial letter is capitalized have meanings defined under the following conditions. The
+        following definitions shall have the same meaning regardless of whether they appear in singular or in plural.
+      </p>
+      <h3>Definitions</h3>
+      <p>For the purposes of this Privacy Policy:</p>
+      <ul>
+        <li>
+          <p>
+            <strong>Company</strong> (referred to as either &quot;the Company&quot;, &quot;We&quot;, &quot;Us&quot; or
+            &quot;Our&quot; in this Agreement) refers to Fr0ntierX Inc., 3500 South Dupont Highway Dover, 19901,
+            Delaware.
+          </p>
+        </li>
+
+        <li>
+          <p>
+            <strong>Country</strong> refers to: Delaware, United States
+          </p>
+        </li>
+        <li>
+          <p>
+            <strong>Device</strong> means any device that can access the Service such as a computer, a cellphone or a
+            digital tablet.
+          </p>
+        </li>
+        <li>
+          <p>
+            <strong>Personal Data</strong> is any information that relates to an identified or identifiable individual.
+          </p>
+        </li>
+        <li>
+          <p>
+            <strong>Service</strong> refers to the Website.
+          </p>
+        </li>
+        <li>
+          <p>
+            <strong>Website</strong> refers to ID Token Demo, accessible from{" "}
+            <a href="https://id-token-demo.fr0ntierx.xyz/" rel="external nofollow noopener" target="_blank">
+              https://id-token-demo.fr0ntierx.xyz/
+            </a>
+          </p>
+        </li>
+        <li>
+          <p>
+            <strong>You</strong> means the individual accessing or using the Service, or the company, or other legal
+            entity on behalf of which such individual is accessing or using the Service, as applicable.
+          </p>
+        </li>
+      </ul>
+      <h2>Collecting and Using Your Personal Data</h2>
+      <h3>Types of Data Collected</h3>
+      <h4>Personal Data</h4>
+      <p>
+        While using Our Service, We may ask You to provide Us with certain personally identifiable information that can
+        be used to contact or identify You. Personally identifiable information may include, but is not limited to:
+      </p>
+      <ul>
+        <li>
+          <p>Email address</p>
+        </li>
+      </ul>
+
+      <h3>Use of Your Personal Data</h3>
+      <p>The Company may use Personal Data for the following purposes:</p>
+      <ul>
+        <li>
+          <p>
+            <strong>To provide our Service</strong>.
+          </p>
+        </li>
+      </ul>
+      <h3>Retention of Your Personal Data</h3>
+      <p>
+        The Company will retain Your Personal Data only for as long as is necessary for the purposes set out in this
+        Privacy Policy. We will retain and use Your Personal Data to the extent necessary to comply with our legal
+        obligations (for example, if we are required to retain your data to comply with applicable laws), resolve
+        disputes, and enforce our legal agreements and policies.
+      </p>
+      <p>
+        The Company will also retain Usage Data for internal analysis purposes. Usage Data is generally retained for a
+        shorter period of time, except when this data is used to strengthen the security or to improve the functionality
+        of Our Service, or We are legally obligated to retain this data for longer time periods.
+      </p>
+      <h3>Delete Your Personal Data</h3>
+      <p>
+        You have the right to delete or request that We assist in deleting the Personal Data that We have collected
+        about You.
+      </p>
+      <p>Our Service may give You the ability to delete certain information about You from within the Service.</p>
+      <p>
+        You may update, amend, or delete Your information at any time by signing in to Your Account, if you have one,
+        and visiting the account settings section that allows you to manage Your personal information. You may also
+        contact Us to request access to, correct, or delete any personal information that You have provided to Us.
+      </p>
+      <p>
+        Please note, however, that We may need to retain certain information when we have a legal obligation or lawful
+        basis to do so.
+      </p>
+      <h3>Security of Your Personal Data</h3>
+      <p>
+        The security of Your Personal Data is important to Us, but remember that no method of transmission over the
+        Internet, or method of electronic storage is 100% secure. While We strive to use commercially acceptable means
+        to protect Your Personal Data, We cannot guarantee its absolute security.
+      </p>
+      <h2>Children's Privacy</h2>
+      <p>
+        Our Service does not address anyone under the age of 13. We do not knowingly collect personally identifiable
+        information from anyone under the age of 13. If You are a parent or guardian and You are aware that Your child
+        has provided Us with Personal Data, please contact Us. If We become aware that We have collected Personal Data
+        from anyone under the age of 13 without verification of parental consent, We take steps to remove that
+        information from Our servers.
+      </p>
+      <p>
+        If We need to rely on consent as a legal basis for processing Your information and Your country requires consent
+        from a parent, We may require Your parent's consent before We collect and use that information.
+      </p>
+      <h2>Links to Other Websites</h2>
+      <p>
+        Our Service may contain links to other websites that are not operated by Us. If You click on a third party link,
+        You will be directed to that third party's site. We strongly advise You to review the Privacy Policy of every
+        site You visit.
+      </p>
+      <p>
+        We have no control over and assume no responsibility for the content, privacy policies or practices of any third
+        party sites or services.
+      </p>
+      <h2>Changes to this Privacy Policy</h2>
+      <p>
+        We may update Our Privacy Policy from time to time. We will notify You of any changes by posting the new Privacy
+        Policy on this page.
+      </p>
+      <p>
+        We will let You know via email and/or a prominent notice on Our Service, prior to the change becoming effective
+        and update the &quot;Last updated&quot; date at the top of this Privacy Policy.
+      </p>
+      <p>
+        You are advised to review this Privacy Policy periodically for any changes. Changes to this Privacy Policy are
+        effective when they are posted on this page.
+      </p>
+      <h2>Contact Us</h2>
+      <p>
+        If you have any questions about these Terms and Conditions, You can{" "}
+        <a style={{ color: "blue" }} href="https://fr0ntierx.com">
+          contact us
+        </a>
+      </p>
+    </div>
+  );
+}
